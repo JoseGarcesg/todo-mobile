6ff44046de461786730cce673decdf6303adb6cf
JoseGarces <jose.garcesg@gmail.com>
2026-03-08 13:22:32 -0500

    Create tasks page and configure main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tasks',
+    loadComponent: () => import('./features/tasks/tasks.page').then( m => m.TasksPage)
   },
 ];
