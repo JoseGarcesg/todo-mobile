0c2d37899a7540df2d60aa0611afd306131149ca
JoseGarces <jose.garcesg@gmail.com>
2026-03-08 14:22:26 -0500

    Add categories addcategory and load functions

--- src/app/features/categories/categories.page.ts
+++ src/app/features/categories/categories.page.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonList, IonLabel, IonCheckbox } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Category } from 'src/app/models/category.model';
+import { CategoryService } from 'src/app/services/category.service';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.page.html',
   styleUrls: ['./categories.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton,
+    CommonModule,
+    FormsModule, IonList, IonLabel, IonCheckbox]
 })
 export class CategoriesPage implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  newCategory = '';
+
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    console.log("ng Oninit category");
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.categories = this.categoryService.getCategories();
+  }
+
+  addCategory(){
+    console.log("add category");
+    if (!this.newCategory) return;
+
+    const category: Category = {
+      id: uuid(),
+      name: this.newCategory
+    };
+
+    this.categoryService.addCategory(category);
+
+    this.newCategory = '';
+    this.loadCategories();  
   }
 
 }
