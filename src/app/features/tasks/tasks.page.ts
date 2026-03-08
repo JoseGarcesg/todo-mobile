370b36c843765d33141c41d6737aea7564346312
JoseGarces <jose.garcesg@gmail.com>
2026-03-08 13:56:43 -0500

    feat: add tasks pages and first feature addTask

--- src/app/features/tasks/tasks.page.ts
+++ src/app/features/tasks/tasks.page.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonList, IonLabel, IonCheckbox } from '@ionic/angular/standalone';
+import { TaskService } from 'src/app/services/task.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.page.html',
   styleUrls: ['./tasks.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton,
+    CommonModule,
+    FormsModule, IonList, IonLabel, IonCheckbox]
 })
-export class TasksPage implements OnInit {
+export class TasksPage implements OnInit{
 
-  constructor() { }
+  tasks:any[] = [];
+  newTask='';
 
-  ngOnInit() {
+  constructor(private taskService:TaskService) {}
+
+   ngOnInit(){
+    this.loadTasks();
+  }
+
+  loadTasks(){
+    console.log('load task');
+    
+    this.tasks = this.taskService.getTasks();
+  }
+
+
+
+  addTask(){
+    if(!this.newTask) return;
+
+    this.taskService.addTask({
+      id:uuid(),
+      title:this.newTask,
+      completed:false
+    });
+
+    this.newTask='';
+    this.loadTasks();
   }
 
 }
